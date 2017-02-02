edb44cbcf63e9a6a2ede2fe84cae73ea26313665
MOUGINOT B <mouginot.baptiste@gmail.com>
2017-02-02 16:17:34 -0600

    working ??

--- build_lhs.py
+++ build_lhs.py
@@ -1,12 +1,29 @@
-import pyDOE import lhs
+#!/usr/bin/env python
 
+import matplotlib
+matplotlib.use('Agg')
 
-
+from pyDOE import lhs
+import numpy as np
+import pandas as pd
+from scipy import stats, integrate
+import matplotlib.pyplot as plt
+import seaborn as sns
+import sys
 
 def main():
-     my_LHS = lhs(10, samples=5000, criterion='maximin', iterations=10)
-    
-    
+    dim = int(sys.argv[1])
+    sample = int(sys.argv[2])
+    it = int(sys.argv[3])
+    method = sys.argv[4]
+
+    my_LHS = lhs(dim, samples=sample, criterion=method, iterations=it)
+    df = pd.DataFrame(my_LHS)
+    g = sns.PairGrid(df)
+    g.map_diag(sns.kdeplot)
+    cmap = sns.cubehelix_palette(as_cmap=True, dark=0, light=1, reverse=True)
+    g.map_offdiag(sns.kdeplot, cmap=cmap, n_levels=60, shade=True);
+    g.savefig("output.png")
 
 if __name__ == "__main__":
         main()
